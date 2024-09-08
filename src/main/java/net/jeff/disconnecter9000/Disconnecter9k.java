bf563e5017de018dadc6999554561d1af3961a6c
WaterSwigger <derpyjeffreypet@gmail.com>
2024-09-08 13:59:43 -0700

    Update to 1.21.1

--- src/main/java/net/jeff/disconnecter9000/Disconnecter9k.java
+++ src/main/java/net/jeff/disconnecter9000/Disconnecter9k.java
@@ -8,6 +8,15 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class Disconnecter9k implements ModInitializer {
 	private static KeyBinding disconnectKeyBinding;
